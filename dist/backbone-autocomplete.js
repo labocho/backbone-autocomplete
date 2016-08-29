37559c50deb359f8f42cd314547589d09ed46290
labocho <labocho@penguinlab.jp>
2016-08-29 16:29:11 +0900

    update dist

--- dist/backbone-autocomplete.js
+++ dist/backbone-autocomplete.js
@@ -72,7 +72,11 @@ Backbone.Autocomplete = {
       url_function = url;
     } else {
       url_function = function url_function() {
-        return url + "?term=" + window.encodeURIComponent(this.query);
+        if (url.match(/\?/)) {
+          return url + "&term=" + window.encodeURIComponent(this.query);
+        } else {
+          return url + "?term=" + window.encodeURIComponent(this.query);
+        }
       };
     }
 
